bb8b665e9a7e5d3eb758b67b9d78e62547440c08
Brian <sukhoonchoi1@gmail.com>
2017-09-08 16:49:43 +1200

    Initial commit

---
+++ src/se206_a03/views/testMenu/RecordingMenu.java
@@ -0,0 +1,16 @@
+package se206_a03.views.testMenu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RecordingMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/se206_a03/views/testMenu/RecordingMenuController.java
@@ -0,0 +1,23 @@
+package se206_a03.views.testMenu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RecordingMenuController {
+
+    @FXML
+    private void difficultyOneButtonAction(ActionEvent event){
+
+        //Change scene into difficulty one
+
+    }
+
+    @FXML
+    private void difficultyTwoButtonAction(ActionEvent event){
+
+        //Change scene into difficulty two
+
+    }
+
+
+}
